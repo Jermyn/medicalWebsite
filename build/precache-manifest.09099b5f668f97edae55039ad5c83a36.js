71933982ba0bd8156c589fa9b7089878a194df71
Jermz <eleljj@nus.edu.sg>
2020-04-30 14:13:29 +0800

    Changes

---
+++ build/precache-manifest.09099b5f668f97edae55039ad5c83a36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/medicalWebsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a05a408ff00a72dce66e",
+    "url": "/medicalWebsite/static/js/runtime~main.a05a408f.js"
+  },
+  {
+    "revision": "3aa3d59273cd74e2785e",
+    "url": "/medicalWebsite/static/js/main.3aa3d592.chunk.js"
+  },
+  {
+    "revision": "972874e89860324b0040",
+    "url": "/medicalWebsite/static/js/1.972874e8.chunk.js"
+  },
+  {
+    "revision": "3aa3d59273cd74e2785e",
+    "url": "/medicalWebsite/static/css/main.76fa269b.chunk.css"
+  },
+  {
+    "revision": "972874e89860324b0040",
+    "url": "/medicalWebsite/static/css/1.66f4e899.chunk.css"
+  },
+  {
+    "revision": "baf34adbea91a669dd32f8dcf703c665",
+    "url": "/medicalWebsite/index.html"
+  }
+];
